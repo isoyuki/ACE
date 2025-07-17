21a1339c56ed068a5341dc0f5e92da6a92688d30
Yuki Kume <git@yukiku.me>
2025-07-17 23:35:09 +1000

    Add Generator

---
+++ src/main/java/org/monash/core/generator/Generator.java
@@ -0,0 +1,17 @@
+package org.monash.core.generator;
+
+import java.util.logging.Logger;
+
+public class Generator {
+
+    private static final Logger LOGGER = Logger.getLogger(Generator.class.getName());
+
+    public static void main(String[] args) {
+
+        // Trapdoor permutation
+
+    }
+}
+
+// What the key size and types are for K, what is the security parameter
+// Notation g^
